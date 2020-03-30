be43343ab21f49c6eccf53f1bc0c75cb17724e86
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-30 08:22:06 +0700

    #14 - Add excelStudentReport in ImportApiController

--- src/main/java/com/hendisantika/springbootexportimportexcelpoi/controller/ImportApiController.java
+++ src/main/java/com/hendisantika/springbootexportimportexcelpoi/controller/ImportApiController.java
@@ -1,9 +1,17 @@
 package com.hendisantika.springbootexportimportexcelpoi.controller;
 
+import com.hendisantika.springbootexportimportexcelpoi.model.Student;
 import com.hendisantika.springbootexportimportexcelpoi.repository.StudentRepository;
 import com.hendisantika.springbootexportimportexcelpoi.utils.ExcelGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +29,18 @@ public class ImportApiController {
 
     @Autowired
     private ExcelGenerator excel;
+
+    @GetMapping("/export")
+    public ResponseEntity<InputStreamResource> excelStudentReport() throws Exception {
+        List<Student> studentList = studentRepository.findAll();
+
+        ByteArrayInputStream in = excel.exportExcel(studentList);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=students.xlsx");
+
+        return ResponseEntity.ok().headers(headers).body(new InputStreamResource(in));
+
+    }
 
 }
