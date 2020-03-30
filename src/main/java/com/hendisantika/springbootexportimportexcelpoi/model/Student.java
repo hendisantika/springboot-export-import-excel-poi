2c1327130a5c3e7df4e936728474d0ba05f5cf60
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-30 08:14:12 +0700

    #6 - Add Student Model

---
+++ src/main/java/com/hendisantika/springbootexportimportexcelpoi/model/Student.java
@@ -0,0 +1,34 @@
+package com.hendisantika.springbootexportimportexcelpoi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-export-import-excel-poi
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 30/03/20
+ * Time: 08.13
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String kelas;
+    private String jurusan;
+}
