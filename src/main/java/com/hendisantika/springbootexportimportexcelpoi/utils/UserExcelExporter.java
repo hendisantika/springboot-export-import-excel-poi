fbbb80a3d823b0bd7decf0a288e9d01d17582a78
Hendi Santika <hendisantika@gmail.com>
2021-09-20 06:56:16 +0700

    #55 - Add writeHeaderLine in UserExcelExporter

--- src/main/java/com/hendisantika/springbootexportimportexcelpoi/utils/UserExcelExporter.java
+++ src/main/java/com/hendisantika/springbootexportimportexcelpoi/utils/UserExcelExporter.java
@@ -1,10 +1,15 @@
 package com.hendisantika.springbootexportimportexcelpoi.utils;
 
 import com.hendisantika.springbootexportimportexcelpoi.model.User;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.util.List;
+
+import static org.apache.poi.ss.util.CellUtil.createCell;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,6 +28,24 @@ public class UserExcelExporter {
     public UserExcelExporter(List<User> listUsers) {
         this.listUsers = listUsers;
         workbook = new XSSFWorkbook();
+    }
+
+    private void writeHeaderLine() {
+        sheet = workbook.createSheet("Users");
+
+        Row row = sheet.createRow(0);
+
+        CellStyle style = workbook.createCellStyle();
+        XSSFFont font = workbook.createFont();
+        font.setBold(true);
+        font.setFontHeight(16);
+        style.setFont(font);
+
+        createCell(row, 0, "User ID", style);
+        createCell(row, 1, "E-mail", style);
+        createCell(row, 2, "Full Name", style);
+        createCell(row, 3, "Roles", style);
+        createCell(row, 4, "Enabled", style);
     }
 
 }
