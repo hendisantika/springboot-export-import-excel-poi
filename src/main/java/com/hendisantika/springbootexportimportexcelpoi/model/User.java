0927173389306f67b1c15d7f77cb4ddcfff9afa5
Hendi Santika <hendisantika@gmail.com>
2021-09-20 06:51:25 +0700

    #50 - Add User Model

---
+++ src/main/java/com/hendisantika/springbootexportimportexcelpoi/model/User.java
@@ -0,0 +1,47 @@
+package com.hendisantika.springbootexportimportexcelpoi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-export-import-excel-poi
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/09/21
+ * Time: 06.47
+ */
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String email;
+
+    private String password;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    private boolean enabled;
+
+    @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+}
