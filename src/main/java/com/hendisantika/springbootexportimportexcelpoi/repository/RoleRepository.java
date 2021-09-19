7957a673a529c988d5087f86f0f399face39cdf0
Hendi Santika <hendisantika@gmail.com>
2021-09-20 06:53:08 +0700

    #52 - Add RoleRepository

---
+++ src/main/java/com/hendisantika/springbootexportimportexcelpoi/repository/RoleRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootexportimportexcelpoi.repository;
+
+import com.hendisantika.springbootexportimportexcelpoi.model.Role;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-export-import-excel-poi
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/09/21
+ * Time: 06.52
+ */
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+}
