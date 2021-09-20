6f6d277eb66a313c61afb65764ff674cea039d58
Hendi Santika <hendisantika@gmail.com>
2021-09-20 07:00:06 +0700

    #60 - Add exportToExcel in UserController

--- src/main/java/com/hendisantika/springbootexportimportexcelpoi/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootexportimportexcelpoi/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.hendisantika.springbootexportimportexcelpoi.controller;
 
 import com.hendisantika.springbootexportimportexcelpoi.service.UserServices;
+import com.hendisantika.springbootexportimportexcelpoi.utils.UserExcelExporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +25,21 @@ public class UserController {
 
     @Autowired
     private UserServices userServices;
+
+    @GetMapping("/users/export/excel")
+    public void exportToExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=users_" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        List<User> listUsers = service.listAll();
+
+        UserExcelExporter excelExporter = new UserExcelExporter(listUsers);
+
+        excelExporter.export(response);
+    }
 }
